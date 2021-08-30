5f60808483644f7a0d5466114c294e21132daad0
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2021-08-30 17:27:03 +0200

    Add option to list available mensas.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use chrono::{DateTime, Local};
 use clap::{AppSettings, Clap};
 use colored::*;
 use serde::Deserialize;
+use std::fmt;
 use std::iter::Iterator;
 use textwrap::{fill, indent};
 
@@ -61,6 +62,26 @@ struct Prices {
     r#extern: Option<String>,
 }
 
+impl fmt::Display for Prices {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let rep = match (
+            self.student.as_ref(),
+            self.staff.as_ref(),
+            self.r#extern.as_ref(),
+        ) {
+            (Some(a), Some(b), Some(c)) => format!("{}/{}/{}", a, b, c),
+            (Some(a), None, None) => a.clone(),
+            (None, Some(b), None) => b.clone(),
+            (None, None, Some(c)) => c.clone(),
+            (Some(a), Some(b), None) => format!("{}/{}", a, b),
+            (None, Some(b), Some(c)) => format!("{}/{}", b, c),
+            (Some(a), None, Some(c)) => format!("{}/{}", a, c),
+            (None, None, None) => "".to_string(),
+        };
+        write!(f, "{}", rep)
+    }
+}
+
 #[derive(Deserialize, Debug)]
 struct Mensa {
     id: u32,
@@ -76,7 +97,48 @@ struct Mensa {
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
     #[clap(default_value = "poly")]
-    input: String,
+    mensa: String,
+    #[clap(long, default_value = "de")]
+    lang: String,
+    #[clap(short, long)]
+    prices: bool,
+    #[clap(short, long)]
+    list: bool,
+}
+
+fn show_available_mensas(mensa_meals: Vec<Mensa>) {
+    for mensa in mensa_meals {
+        println!("{}", mensa.mensa)
+    }
+}
+
+fn show_meals_for_mensa(mensa_meals: Vec<Mensa>, mensa_name: &str, show_prices: bool) {
+    match mensa_meals
+        .iter()
+        .find(|&m| m.mensa.to_lowercase().contains(&mensa_name.to_lowercase()))
+    {
+        Some(mensa) => {
+            for meal in &mensa.meals {
+                println!("{}", meal.label.bold());
+                println!("{}", meal.description[0]);
+                for i in 1..meal.description.len() {
+                    if !meal.description[i].is_empty() {
+                        println!(
+                            "{}",
+                            indent(fill(meal.description[i].as_str(), 40).as_str(), "    ")
+                        );
+                    }
+                }
+                if show_prices {
+                    println!("\t\t\t\t{}", meal.prices);
+                }
+                println!();
+            }
+        }
+        None => {
+            println!("Could not find mensa, it might be closed today.");
+        }
+    };
 }
 
 #[tokio::main]
@@ -84,13 +146,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
 
     let baseurl = "https://www.webservices.ethz.ch/gastro/v1/RVRI/Q1E1";
-    let lang = "en";
     let localtime: DateTime<Local> = Local::now();
 
     let meals_url = format!(
         "{baseurl}/meals/{lang}/{date}/{time}",
         baseurl = baseurl,
-        lang = lang,
+        lang = opts.lang,
         date = format!("{}", localtime.format("%F")),
         time = "lunch",
     );
@@ -98,27 +159,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let meals_response = reqwest::get(&meals_url).await?;
     let mensa_meals: Vec<Mensa> = meals_response.json().await?;
 
-    match mensa_meals
-        .iter()
-        .find(|&m| m.mensa.to_lowercase().contains(&opts.input.to_lowercase()))
-    {
-        Some(mensa) => {
-            for meal in &mensa.meals {
-                println!("{}", meal.label.bold());
-                println!("{}", meal.description[0]);
-                for i in 1..meal.description.len() {
-                    println!(
-                        "{}",
-                        indent(fill(meal.description[i].as_str(), 40).as_str(), "    ")
-                    );
-                }
-                println!("");
-            }
-        }
-        None => {
-            println!("Could not find mensa, it might be closed today.");
-        }
-    };
-
+    if opts.list {
+        show_available_mensas(mensa_meals);
+    } else {
+        show_meals_for_mensa(mensa_meals, &opts.mensa, opts.prices);
+    }
     Ok(())
 }
